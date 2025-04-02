964881387f8aaee5c4450dcbb6f3bc97ea010504
TheMarineLord2 <jbullmann10+github@gmail.com>
2025-04-02 21:47:28 +0200

    Moved from Maven to Gradle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,8 @@
 package org.example;
 import org.example.mainControllers.mainScreen.MainOperator;
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class Main {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
         MainOperator mainOperator = MainOperator.getInstance();
     }
